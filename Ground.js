40a4127699ea9423b0fe9f9e9f0290ceac482e19
GanaTeja <64629231+GanaTeja@users.noreply.github.com>
2020-05-07 11:00:50 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,40 @@
+class Ground{
+constructor (){
+
+
+    var options = {
+        isStatic:true
+    }
+    this.body = Bodies.rectangle(200, 390, 400,10,options);
+    this.width = 400;
+    this.height = 10;
+    
+    World.add(world, this.body);
+}
+    display(){
+        var pos =this.body.position;
+        rectMode(CENTER);
+        fill("yellow");
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+      }
+
+    }; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
